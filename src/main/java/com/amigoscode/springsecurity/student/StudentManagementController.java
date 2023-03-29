7f133c132425d74ab545400a6c66758ca15a4020
Srizan <write2srizan@gmail.com>
2023-03-29 17:45:42 +0600

    implemented permission based authentication using annotation

--- src/main/java/com/amigoscode/springsecurity/student/StudentManagementController.java
+++ src/main/java/com/amigoscode/springsecurity/student/StudentManagementController.java
@@ -1,6 +1,7 @@
 package com.amigoscode.springsecurity.student;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -16,25 +17,31 @@ public class StudentManagementController {
             new Student(3, "Anna Smith")
     );
 
+    //hasRole('ROLE_') hasAnyRole('ROLE_') hasAuthority('permission') hasAnyAuthority('permission')
+
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMIN_TRAINEE')")
     public List<Student> getAllStudents() {
         System.out.println("getAllStudents");
         return students;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println("registerNewStudent");
         System.out.println(student);
     }
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println("deleteStudent");
         System.out.println(studentId);
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         System.out.println("updateStudent");
         System.out.println(String.format("%s %s", studentId, student));
