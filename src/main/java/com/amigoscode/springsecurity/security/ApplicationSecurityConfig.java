f92ee52730eb494bcdd387ffb1577500356068e6
Srizan <write2srizan@gmail.com>
2023-03-30 18:27:04 +0600

    implemented form based authentication

--- src/main/java/com/amigoscode/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/amigoscode/springsecurity/security/ApplicationSecurityConfig.java
@@ -3,7 +3,6 @@ package com.amigoscode.springsecurity.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,8 +12,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
-import static com.amigoscode.springsecurity.security.ApplicationUserPermission.COURSE_WRITE;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import java.util.concurrent.TimeUnit;
 import static com.amigoscode.springsecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -37,14 +36,28 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMIN_TRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .defaultSuccessUrl("/courses", true)
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("secret-key")
+                    .rememberMeParameter("remember-me")
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID","remember-me")
+                    .logoutSuccessUrl("/login");
 
     }
 
@@ -71,7 +84,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 //.roles(ADMIN_TRAINEE.name())
                 .authorities(ADMIN_TRAINEE.getGrantedAuthorities())
                 .build();
-
 
         return new InMemoryUserDetailsManager(
                 annasmith,
